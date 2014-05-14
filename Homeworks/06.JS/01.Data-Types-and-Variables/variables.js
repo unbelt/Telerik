b6d76faf72001aa4dcbf902e932e02b7cfb05b13
unbelt <unbelt@outlook.com>
2014-05-15 00:14:18 +0300

    JavaScript
    Data Types and Variables

---
+++ Homeworks/06.JS/01.Data-Types-and-Variables/variables.js
@@ -0,0 +1,26 @@
+// 01. Assign all the possible JavaScript literals to different variables.
+var stringVar = "Hello World!";
+
+var numberOne = 413;
+var numberTwo = 3.14;
+
+var boolVar = true; // true or false
+
+var nullVar = null;
+var undefinedVar = undefined;
+
+
+// 02. Create a string variable with quoted text in it. For example: 'How you doin'?', Joey said.
+var escaping = "\'How you doin\'?\', Joey said.";
+
+
+// 03.Try typeof on all variables you created.
+typeof(stringVar);
+typeof(numberOne);
+typeof(numberTwo);
+typeof(boolVar);
+
+
+// 04. Create null, undefined variables and try typeof on them.
+typeof(nullVar);
+typeof(undefinedVar);
